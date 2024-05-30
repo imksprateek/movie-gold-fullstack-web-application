1732be7d88ec1fb04beaeb89f2513cd3a11141ba
imksprateek <ksprateek2004@gmail.com>
2024-05-30 20:53:22 +0530

    Completed backend application and Initialized frontend

---
+++ Backend - MoviesAPI/MoviesAPI/src/main/java/me/ksprateek/movies/MovieRepository.java
@@ -0,0 +1,11 @@
+package me.ksprateek.movies;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, Object> {
+    Optional<Movie> findMovieByImdbId(String imdbId);
+}
